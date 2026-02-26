a9fcd62d8d262aa6855d1c0ea107dfaa6eb502a8
Rizalputra5 <risalmaulanamaulana@gmail.com>
2026-02-26 20:38:19 +0700

    Fix loadstring URL formatting in main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,1 @@
---// MAIN.LUA SIMPLE
-local user = "rizalputraaa"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/"..user.."/riiicuy-hub/main/engine.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/rizalputraaa/riiicuy-hub/main/engine.lua"))()
